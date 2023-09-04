f8e44d19a7a90f64376ff08c05f7d4599e05e66c
lee juyoung <juyung0903@naver.com>
2023-09-05 01:34:52 +0900

    chore: 라우팅

--- Hanium-Project/src/App.tsx
+++ Hanium-Project/src/App.tsx
@@ -2,9 +2,17 @@ import { useState } from 'react';
 import MainPage from '@/page/MainPage';
 import MonitoringPage from '@/page/MonitoringPage';
 import DocumentPage from '@/page/DocumentPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <MonitoringPage />;
-}
+  return (
 
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/monitoring" element={<MonitoringPage />} />
+        <Route path="/document" element={<DocumentPage />} />
+      </Routes>
+
+  );
+}
 export default App;
--- Hanium-Project/src/components/NavBar.tsx
+++ Hanium-Project/src/components/NavBar.tsx
@@ -1,38 +1,46 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import Button from './Button';
-import LoginModal from './LoginModal';
-import SignUpModal from './SignUpModal';
+import LoginModal from './Modal/LoginModal';
+import SignUpModal from './Modal/SignUpModal';
 import { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 function NavBar() {
   const [loginClicked, setLoginClicked] = useState<boolean>(false);
+  const [modal, setIsModal] = useState<Number>(0);
+  const navigate = useNavigate();
   return (
     <div className="w-full bg-white h-24 flex justify-between items-center">
       <div className="w-1/6 h-full bg-indigo"></div>
-      <div className="w-3/5 flex justify-evenly items-center h-full cursor-pointer text-lg font-medium ">
-        <div className="group w-32 h-10 flex justify-center items-center hover:text-sky-500">
+      <div className="w-3/5 flex justify-evenly items-center h-full cursor-pointer text-xl font-medium ">
+        <div className="group w-32 h-10 flex justify-center items-center hover:text-sky-500" onClick={()=>{navigate('/')}}>
           <div className="w-fit h-8 flex flex-col justify-center">메인</div>
         </div>
-        <div className="w-32 text-center hover:text-sky-500">모니터링</div>
-        <div className="w-32 text-center hover:text-sky-500">기록</div>
+        <div className="w-32 text-center hover:text-sky-500" onClick={()=>{navigate('/monitoring')}}>모니터링</div>
+        <div className="w-32 text-center hover:text-sky-500" onClick={()=>{navigate('/document')}}>기록</div>
         <div className="w-32 text-center hover:text-sky-500">물류 현황</div>
       </div>
       <div className="w-1/6 h-full flex justify-evenly items-center">
         <Button
-          width="24"
-          height="12"
-          text="Login"
-          bg="rgb(59 130 246)"
+          width="80px"
+          height="10"
+          text="로그인"
+          bg="rgb(96 165 250)"
           clickHandler={() => {
-            setLoginClicked(true);
+            setIsModal(1);
           }}
         />
         <FontAwesomeIcon
-          className="text-4xl text-cyan-600 cursor-pointer"
+          className="text-3xl text-blue-400 cursor-pointer"
           icon={faBell}
         />
       </div>
-      {loginClicked && <LoginModal />}
+      {modal === 1 && (
+        <LoginModal Modal={modal} setIsModal={setIsModal} />
+      )}
+      {modal === 2 && (
+        <SignUpModal Modal={modal} setIsModal={setIsModal} />
+      )}
     </div>
   );
 }
